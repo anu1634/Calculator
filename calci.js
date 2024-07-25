e0fa9562c44f9880f6c2d9d1b41dbc4ab604a090
anu1634 <anu1232004@gmail.com>
2024-07-25 16:23:34 +0530

    first commit

---
+++ calci.js
@@ -0,0 +1,22 @@
+function fn(param){
+    if(param == '+' || param == '-' || param == '%' || param == '/' || param == '*'){
+        document.getElementById("typed").innerHTML += param;
+        document.getElementById("typing").innerHTML ='';
+    } 
+    else{
+        document.getElementById("typed").innerHTML += param;
+        document.getElementById("typing").innerHTML += param;
+    }
+}
+
+function result(){
+    let res = eval(document.getElementById("typed").innerHTML)
+    document.getElementById("typing").innerHTML = res
+}
+function c(){
+    document.getElementById("typing").innerHTML = '0'
+}
+function ce(){
+    document.getElementById("typing").innerHTML ='0'
+    document.getElementById("typed").innerHTML = ''
+}
